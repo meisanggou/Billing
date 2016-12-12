f17e45ff8d37ed68b47f7e2bc7aafbf9a7c5eb3a
meisanggou <zhou5315938@163.com>
2016-12-12 08:34:17 +0800

    修改html

--- Web/views/charge_view.py
+++ Web/views/charge_view.py
@@ -4,7 +4,7 @@ __author__ = 'ZhouHeng'
 
 from flask import g, jsonify, request
 from Tools.RenderTemplate import RenderTemplate
-from Web import create_blue, charge_url_prefix as url_prefix, project_required
+from Web import create_blue, charge_url_prefix as url_prefix, billing_item_url_prefix
 from Web import control
 
 
@@ -14,5 +14,6 @@ charge_view = create_blue("charge_view", url_prefix=url_prefix)
 
 @charge_view.route("/", methods=["GET"])
 def add_charge_page_func():
-    return rt.render("charge.html")
+    item_url = billing_item_url_prefix + "/"
+    return rt.render("charge.html", item_url=item_url)
 
